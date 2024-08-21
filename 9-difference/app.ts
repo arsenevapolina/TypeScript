8304b04d8a41c27200b6f355abe220352468aafc
Полина <141886064+arsenevapolina@users.noreply.github.com>
2024-08-21 03:06:04 +0300

    Изменено наименование interfaces

--- 9-difference/app.ts
+++ 9-difference/app.ts
@@ -1,9 +1,9 @@
-interface IA {
+interface A {
   a: number;
   b: string;
 }
 
-interface IB {
+interface B {
   a: number;
   c: boolean;
 }
@@ -26,8 +26,8 @@ function difference<T extends object, Y extends object>(
   return result;
 }
 
-let a: IA = { a: 5, b: "" };
-let b: IB = { a: 10, c: true };
+let a: A = { a: 5, b: "" };
+let b: B = { a: 10, c: true };
 
 const result = difference(a, b);
 console.log(result);
